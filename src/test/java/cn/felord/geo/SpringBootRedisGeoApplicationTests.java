2d449b3ac50f2caa48fb2d917bc85a36c514a7b6
felord <felord@qq.com>
2020-06-18 16:21:23 +0800

    first commit

---
+++ src/test/java/cn/felord/geo/SpringBootRedisGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.felord.geo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisGeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
